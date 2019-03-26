61d67284aab9696095cd7085b6ba2b38e785012b
Blak3Nick <blaken2021@gmail.com>
2019-03-26 14:31:28 -0400

    working with python

--- assignMembers.py
+++ assignMembers.py
@@ -1,4 +1,11 @@
-class AssignMembers:
-    def assignAllMembers(self):
-        mn = MainClass(self)
-        filePath = mn.get_path(self)
+
+def assign_members(file_to_read):
+    members = list()
+    with open(file_to_read, 'r') as f:
+        for line in f:
+            members.append(line.rstrip('\n\t '))
+    return members
+
+
+
+
--- mainApp.py
+++ mainApp.py
@@ -1,25 +1,36 @@
+
 from tkinter import *
 from tkinter import ttk
+from assignMembers import assign_members
 
-main_file_path = 'C:\\Users\\Cyr1lfiggus1\\PycharmProjects\\SquadronReadinessPython'
+main_file_path = 'C:\\Users\\Cyr1lfiggus1\\PycharmProjects\\SquadronReadinessPython\\'
 
 
-class MainClass(object):
-    def __init__(self):
-        pass
-
-    def get_path(self):
-        return main_file_path
 
 
 def enable_text():
     firstEntry.config(state='normal')
     lastEntry.config(state='normal')
 
+all_sfs_members = assign_members(main_file_path+'allSFSMembers.txt')
+squad1 = assign_members(main_file_path+'SQUAD1.txt')
+squad2 = assign_members(main_file_path+'SQUAD2.txt')
+fire11 = assign_members(main_file_path+'fire1-1.txt')
+fire12 = assign_members(main_file_path+'fire1-2.txt')
+fire13 = assign_members(main_file_path+'fire1-3.txt')
+fire14 = assign_members(main_file_path+'fire1-4.txt')
+fire21 = assign_members(main_file_path+'fire2-1.txt')
+fire22 = assign_members(main_file_path+'fire2-2.txt')
+fire23 = assign_members(main_file_path+'fire2-3.txt')
+fire24 = assign_members(main_file_path+'fire2-4.txt')
+
+print(squad1)
+
+
 
 root = Tk()
 frame = Frame(root)
-a = IntVar()
+a = StringVar(value='indv')
 labelText = StringVar()
 Label(root, text='First Name').grid(row=3, sticky=W, padx=4)
 firstEntry = Entry(root)
@@ -45,13 +56,21 @@ Radiobutton(root, variable=a, text='Fire 2-3', value='fire23').grid(row=2, colum
 Radiobutton(root, variable=a, text='Fire 2-4', value='fire24').grid(row=2, column=3)
 
 
+def get_button_selected(a):
+    selection = a.get()
+    print('button pushed', selection)
 
 
-Button(root, text='Process reports').grid(row=5)
+process_button = Button(root, text='Process reports', command=lambda: get_button_selected(a))
+process_button.grid(row=5)
 # button = Button(frame, text='b1').pack(side=LEFT, fill=Y)
 # button = Button(frame, text='b2').pack(side=TOP, fill=X)
 # button = Button(frame, text='b3').pack(side=RIGHT, fill=X)
 # button = Button(frame, text='b4').pack(side=BOTTOM, fill=X)
+
+
+
+
 
 root.title('Squadron Readiness')
 root.mainloop()
